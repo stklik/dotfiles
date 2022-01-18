2eece80a8d77a94efcb31fa4d4d0b9942e4a393d
Stefan Klikovits <stefan@klikovits.net>
2022-01-18 14:16:28 +0900

    first version of home-manager

---
+++ home.nix
@@ -0,0 +1,193 @@
+{ config, pkgs, ... }:
+let
+  bash_profile_content =
+    ''
+    # Load the shell dotfiles, and then some:
+    # * ~/.paths can be used to extend `$PATH`.
+    # * ~/.extra can be used for other settings you don’t want to commit.
+    for file in ~/.{paths,aliases,exports,functions,extra}; do
+      [ -r "$file" ] && [ -f "$file" ] && source "$file";
+    done;
+    unset file;
+    '';
+
+  comma = import ( pkgs.fetchFromGitHub {
+      owner = "nix-community";
+      repo = "comma";
+      rev = "4a62ec17e20ce0e738a8e5126b4298a73903b468";
+      sha256 = "0n5a3rnv9qnnsrl76kpi6dmaxmwj1mpdd2g0b4n1wfimqfaz6gi1";
+  }) {};
+in {
+  home = {
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    username = "stefan";
+    homeDirectory = "/Users/stefan";
+
+    # symlink all dotfiles !
+    file = {
+      ".aliases".source = ./.aliases;
+      ".exports".source = ./.exports;
+      ".functions".source = ./.functions;
+      ".paths".source = ./.paths;
+
+      ".finicky.js".source = ./.finicky.js;  # only interesting on Mac...
+      ".tmux.conf".source = ./.tmux.conf;
+      ".vimrc".source = ./.vimrc;
+    };
+
+    # No-config packages to install
+    packages = with pkgs; [
+      comma
+      curl
+      gitAndTools.delta
+      graphviz
+      mas
+      rsync
+      tldr
+      tree
+      wget
+      zsh-z
+    ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  stateVersion = "22.05";
+
+  };
+
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+    bat.enable = true;
+
+    bash = {
+      enable = true;
+      bashrcExtra =
+        ''
+        # dotfiles loading
+        source ~/.bash_profile;
+        '';
+      profileExtra = bash_profile_content;
+    };
+
+    direnv.enable = true;
+    direnv.nix-direnv.enable = true;
+
+    git = {
+      enable = true;
+      userName = "Stefan Klikovits";
+      userEmail = "stefan@klikovits.net";
+      ignores = [
+        ".DS_Store"
+        "**/.DS_Store"
+        ".AppleDouble"
+        ".LSOverride"
+        "*.niu"
+        ".local"
+
+        # --- From Stefan's .gitconfig --- #
+        # Folder view configuration files
+        "Desktop.ini"
+
+        # Thumbnail cache files
+        "._*"
+        "Thumbs.db"
+
+        # Files that might appear on external disks
+        ".Spotlight-V100"
+        ".Trashes"
+
+        # Always-ignore extensions
+        "*~"
+        "*.diff"
+        "*.err"
+        "*.log"
+        "*.orig"
+        "*.pyc"
+        "*.rej"
+        "*.sass-cache"
+        "*.sw?"
+        "*.vi"
+      ];
+      aliases = {};
+      extraConfig = {
+        # core.pager = "diff-so-fancy | less --tabs=1,5 -R";
+        core.pager = "delta";
+        core.editor = "vim";
+        submodule.recurse = "true";
+        delta.features = "side-by-side line-numbers";
+        github.user = "stklik";
+        init.defaultBranch = "main";
+        pull.rebase = "false";
+      };
+
+    };
+
+    htop.enable = true;
+    jq.enable = true;
+
+    # NOTE: Extra config from dotfile!
+    tmux = {
+      enable = true;
+      baseIndex = 1;
+      historyLimit = 102400;
+
+    };
+
+    # NOTE: Extra config from dotfile!
+    vim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        vim-sensible
+        nerdtree
+        nerdtree-git-plugin
+        vim-fugitive
+        vim-gitgutter
+        vim-airline
+        vim-airline-themes
+        vim-colors-solarized
+        vim-surround
+        vimtex
+        indentLine
+      ];
+      settings = {
+        background = "dark";
+
+        backupdir = [ "~/.vim/backups" ];
+        directory = [ "~/.vim/swaps" ];
+        undodir = [ "~/.vim/undo" ];
+
+        expandtab = true;
+        ignorecase = true;
+        mouse = "a";
+        number = true;
+        relativenumber = true;
+        shiftwidth = 4;
+        tabstop = 4;
+      };
+    };
+
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      autocd = true;
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins=["direnv" "docker" "extract" "gitignore" "macos" "pip" "python" "tmux" "transfer" "z"];
+      };
+
+      initExtra = bash_profile_content;
+    };
+  };
+
+}
